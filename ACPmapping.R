17e178ebc0ed332849e608c745c574ec8be6cd0b
Erik Osnas <erik_osnas@fws.gov>
2022-04-25 16:20:22 -0800

    added function to make linestring sf object

--- ACPmapping.R
+++ ACPmapping.R
@@ -4,27 +4,32 @@ library(tidyverse)
 
 acp <- st_read(dsn="Design_Strata/ACP_2007to2018_DesignStrata.shp") %>%
   st_transform(crs=4326)
-plot(st_geometry(acp))
+plot(st_geometry(acp), col=factor(acp$STRATNAME))
 acp2 <- st_read(dsn="Design_Strata/ACP_2007to2019_StudyArea.shp") %>%
   st_transform(crs=4326)
 plot(st_geometry(acp2))
 
 sum(st_area(acp))
 st_area(acp2)
-#stupid! we should delete the 'study area' file
-acp3 <- st_union(acp)
+# stupid! we should delete the 'study area' file; why do we keep these separate?
+acp3 <- acp %>% st_union() %>% st_make_valid()
 plot(st_geometry(acp3))
+st_area(acp3) #humm, error in area calculations, keep acp2?
+#acp strata overlap so the sum of strata areas should not add up
+#then why does area of acp == acp2?
 
-#make bounding box
+#make bounding box sf object
 bbox <- st_sf(st_as_sfc(st_bbox(acp)))
 
+#read and plot transects
 trans1 <- st_read(dsn="Design_Transects/ACP_2019_Transects.shp") %>%
   st_transform(crs=4326)
 
 ggplot(data=acp) + 
   geom_sf(aes(fill=STRATNAME)) + 
   geom_sf(data=trans1) +
-  guides(fill=guide_legend(title="Strata"))
+  guides(fill=guide_legend(title="Strata")) + 
+  labs(title=2019)
 
 #add a leaflet plot
 library(tmap)
@@ -38,10 +43,15 @@ tm_shape(acp) + tm_polygons(col = "STRATNAME", alpha = 0.5) +
 csv_files = list.files(path = 'Data/', pattern = "csv$", full.names = TRUE)
 # Read each csv file into a list
 birds <- map(csv_files, read_csv, col_types=cols(Transect=col_double())) %>% 
-  map_dfr(select, "Species", "Year", "Lat", "Lon", "Num", "Obs_Type", "Transect") %>%
+  map_dfr(select, "Year", "Month", "Day", "Time", "Transect", 
+          "Observer", "Species", "Num", "Obs_Type", "Lat", "Lon") %>%
   st_as_sf(coords=c("Lon", "Lat"), crs=4326) %>%
   st_filter(bbox) #get rid of the birds in Nigeria (lat/long = 0)
 
+ibp <- birds %>% filter(Obs_Type != "flkdrake", Num > 0,
+                        Obs_Type %in% c("single", "pair") | (Obs_Type == "open" & Num < 3))
+
+flocks <- birds %>% filter(Obs_Type == "flkdrake" | (Obs_Type == "open" & Num >= 3))
 
 Spp <- c("ARTE", "BRAN", "CCGO", "COEI", "GLGU", "GWFG", "JAEG", "KIEI", "LTDU", 
          "NOPI", "PALO", "RTLO", "SAGU", "SNGO", "SPEI", "STEI", "SWAN", "YBLO") 
@@ -51,16 +61,19 @@ Spp <- c("ARTE", "BRAN", "CCGO", "COEI", "GLGU", "GWFG", "JAEG", "KIEI", "LTDU",
 # ge <- c("BRAN", "CCGO", "GWFG", "SNGO", "SWAN")
 # du <- c("AMWI", "LTDU", "NOPI", "RBME")
 
+#singles and pairs == indicated breeding birds
 tm <- tm_shape(acp) + tm_polygons(col = "STRATNAME", alpha = 0.5) + 
-  tm_shape(trans1) + 
-  tm_basemap(server = "Esri.WorldImagery")
-
+  tm_shape(trans1) + tm_lines() +
+  tm_basemap(server = "Esri.WorldGrayCanvas")
 for(i in Spp){
-sp <- filter(birds, Species %in% i) %>% st_as_sf(coords=c("Lon", "Lat"), crs=4326)
+sp <- filter(ibp, Species %in% i) %>% st_as_sf(coords=c("Lon", "Lat"), crs=4326)
 tm <- tm + tm_shape(sp, name=i) + 
   tm_dots(col=terrain.colors(length(Spp))[which(factor(Spp)==i)])
 }
 tm 
+
+#flocks?
+
 
 #map year-specific observations and make track lines
 Y = 2019
@@ -69,18 +82,69 @@ df <- filter(birds, Year==Y) %>%
 ggplot(data=trans1, group=TRANSID) + 
   geom_sf(col=trans1$TRANSID) + 
   geom_sf(data=df, size=0.2)
+#leaflet
+tm <- tm_shape(acp) + tm_polygons(col = "STRATNAME", alpha = 0.5) + 
+  tm_shape(trans1, name="Design Trans.") + tm_lines() +
+  tm_text("TRANSID") +
+  tm_basemap(server = "Esri.WorldGrayCanvas") + 
+  tm_scale_bar()
 
-#try Y = XXXX
-Y = 2007
-trans1 <- st_read(dsn=paste("Design_Transects/ACP_",Y,"_Transects.shp", sep="")) %>%
-  st_transform(crs=4326) 
-df <- filter(birds, Year==Y) %>%
-  group_by(Transect) 
-ggplot(data=trans1, group=TRANSID) + 
-  geom_sf(col=trans1$TRANSID) + 
-  geom_sf_text(aes(label =TRANSID),size=3,family="sans") +
-  geom_sf(data=df, size=0.2) + 
-  labs(title=Y)
+for(Y in 2007:2019){
+sp <- filter(birds, Year == Y) %>% st_as_sf(coords=c("Lon", "Lat"), crs=4326)
+tm <- tm + tm_shape(sp, name=as.character(Y)) + 
+    tm_dots(col=terrain.colors(13)[which(2007:2019==Y)])
+}
+tm 
+
+#make a linestring for each transect
+#first test on 2016 data
+test0 <- filter(birds, Year==2016) %>% 
+  cbind(st_coordinates(.)) %>%
+  #distinct(X, Y) %>%
+  group_by(Transect) %>%
+  arrange(X, .by_group=TRUE) 
+
+#make a linestring from points, first remove geometry, then rebuild
+test <- as.data.frame(test0) %>% 
+  select(-geometry) %>%
+  filter(Transect == 39) %>%
+  arrange(X, Y) %>%
+  select(X, Y) %>%
+  as.matrix() %>%
+  st_linestring() %>%
+  st_sfc(crs=4326) %>%
+  st_sf(geometry=.) %>%
+  mutate(Transect = 39)
+plot(st_geometry(test))
+#that worked, now turn into function and apply across all transects
+
+points2line <- function(x, id=NA, crs=4326){
+  #this function accepts sf object of points and returns an sf linestring
+  #also will accept an ID variable and return that as an attribute of the linestring, 
+  # otherwise returns an ID == NA.
+  linestring <- x %>% cbind(st_coordinates(.)) %>%
+    as.data.frame() %>% 
+    select(-geometry) %>%
+    arrange(X, Y) %>%
+    select(X, Y) %>%
+    as.matrix() %>%
+    st_linestring() %>%
+    st_sfc(crs=4326) %>%
+    st_sf(geometry=.) %>%
+    mutate(id = id)
+  return(linestring)
+}
+
+#test
+x = filter(birds, Year == 2016, Transect == 39)
+points2line(x=x, id=39)
+  
+#plot in leaflet
+tm_shape(acp) + tm_polygons(col = "STRATNAME", alpha = 0.5) + 
+  tm_shape(test, name="Flown Track") + tm_lines() +
+  tm_shape(test0, name="Obs") + tm_dots("Time") +
+  tm_basemap(server = "Esri.WorldGrayCanvas") + 
+  tm_scale_bar() 
 
 #segmentize transects
 source("trans2seg.R")
