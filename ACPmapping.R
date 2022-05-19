03b1f96274f99e5525b2d49be4ab60cfe9013883
Erik Osnas <erik_osnas@fws.gov>
2022-05-19 11:55:46 -0800

    start Quality control process on transects; added comments;

--- ACPmapping.R
+++ ACPmapping.R
@@ -1,33 +1,56 @@
+# This code is meant to explore the ACP data visually to discover QC issues that need correction.
+# The goal here is to produce lines from the observed bird point locations to reconstruct transect effort
+# These lines can then be 'segmented' to produce spatially explicit small-scale sample units for modeling.
+# This code is largely developmental and exploratory
+# written by Erik Osnas, May, 2022
 #load and map ACP
 library(sf)
 library(tidyverse)
 
+#read and plot basic spatial layers
+#design strata
 acp <- st_read(dsn="Design_Strata/ACP_2007to2018_DesignStrata.shp") %>%
-  st_transform(crs=4326)
+  st_transform(crs=3338)
 plot(st_geometry(acp), col=factor(acp$STRATNAME))
+#study area (not sure what this is or how different than above)
 acp2 <- st_read(dsn="Design_Strata/ACP_2007to2019_StudyArea.shp") %>%
-  st_transform(crs=4326)
-plot(st_geometry(acp2))
+  st_transform(crs=3338)
+plot(st_geometry(acp2)) #seem to be outer perimeter of design strata
 
 sum(st_area(acp))
+#58201582314 [m^2]
 st_area(acp2)
+#58201582314 [m^2]
 # stupid! we should delete the 'study area' file; why do we keep these separate?
 acp3 <- acp %>% st_union() %>% st_make_valid()
 plot(st_geometry(acp3))
-st_area(acp3) #humm, error in area calculations, keep acp2?
-#acp strata overlap so the sum of strata areas should not add up
-#then why does area of acp == acp2?
+st_area(acp3) 
+#58201582314 [m^2]
+#acp2 is unecessary, delete
+rm(acp2)
+#All of above are originally in EPSG:3338, if left in 3338, 
+# then area is the same for all of the above
+# on reprojection, error in area calculations arise
 
 #make bounding box sf object
 bbox <- st_sf(st_as_sfc(st_bbox(acp)))
 
-#read and plot transects
-trans1 <- st_read(dsn="Design_Transects/ACP_2019_Transects.shp") %>%
-  st_transform(crs=4326)
+#read and plot transects, here just 2019 for example reference
+trans1 <- st_read(dsn="Design_Transects/ACP_2019_Transects.shp") %>% 
+#this is in EPGS:4269 = lat, long, but the length attributes 
+#  seem to be calculated in 3338 --> transform
+ st_transform(crs=3338)
 
 ggplot(data=acp) + 
   geom_sf(aes(fill=STRATNAME)) + 
   geom_sf(data=trans1) +
+  guides(fill=guide_legend(title="Strata")) + 
+  labs(title=2019) + 
+  coord_sf(datum = st_crs(3338))
+
+ggplot(data=st_transform(acp, crs=st_crs(4326))) + 
+  geom_sf(aes(fill=STRATNAME)) + 
+  geom_sf(data=st_transform(trans1, crs=st_crs(4326))) +
   guides(fill=guide_legend(title="Strata")) + 
   labs(title=2019)
 
@@ -38,90 +61,91 @@ tm_shape(acp) + tm_polygons(col = "STRATNAME", alpha = 0.5) +
   tm_shape(trans1) + tm_lines() + 
   tm_basemap(server = "Esri.WorldImagery")
   
-
 #add bird observation
 csv_files = list.files(path = 'Data/', pattern = "csv$", full.names = TRUE)
 # Read each csv file into a list
 birds <- map(csv_files, read_csv, col_types=cols(Transect=col_double())) %>% 
   map_dfr(select, "Year", "Month", "Day", "Time", "Transect", 
-          "Observer", "Species", "Num", "Obs_Type", "Lat", "Lon") %>%
+          "Observer", "Species", "Num", "Obs_Type", "Code", "Lat", "Lon") %>%
   st_as_sf(coords=c("Lon", "Lat"), crs=4326) %>%
-  st_filter(bbox) #get rid of the birds in Nigeria (lat/long = 0)
+  st_transform(crs=3338) %>%
+  st_filter(bbox) #get rid of the birds in Nigeria (lat/long = 0), only 14 observations
 
-ibp <- birds %>% filter(Obs_Type != "flkdrake", Num > 0,
-                        Obs_Type %in% c("single", "pair") | (Obs_Type == "open" & Num < 3))
-
-flocks <- birds %>% filter(Obs_Type == "flkdrake" | (Obs_Type == "open" & Num >= 3))
-
-Spp <- c("ARTE", "BRAN", "CCGO", "COEI", "GLGU", "GWFG", "JAEG", "KIEI", "LTDU", 
-         "NOPI", "PALO", "RTLO", "SAGU", "SNGO", "SPEI", "STEI", "SWAN", "YBLO") 
+# ibp <- birds %>% filter(Obs_Type != "flkdrake", Num > 0,
+#                         Obs_Type %in% c("single", "pair") | (Obs_Type == "open" & Num < 3))
+# 
+# flocks <- birds %>% filter(Obs_Type == "flkdrake" | (Obs_Type == "open" & Num >= 3))
+# 
+# Spp <- c("ARTE", "BRAN", "CCGO", "COEI", "GLGU", "GWFG", "JAEG", "KIEI", "LTDU", 
+#          "NOPI", "PALO", "RTLO", "SAGU", "SNGO", "SPEI", "STEI", "SWAN", "YBLO") 
 # lo <- c("PALO", "RTLO", "YBLO")
 # ei <- c("COEI", "KIEI", "SPEI", "STEI")
 # gu <- c("ARTE", "GLGU", "JAEG", "SAGU")
 # ge <- c("BRAN", "CCGO", "GWFG", "SNGO", "SWAN")
 # du <- c("AMWI", "LTDU", "NOPI", "RBME")
-
+# 
 #singles and pairs == indicated breeding birds
-tm <- tm_shape(acp) + tm_polygons(col = "STRATNAME", alpha = 0.5) + 
-  tm_shape(trans1) + tm_lines() +
-  tm_basemap(server = "Esri.WorldGrayCanvas")
-for(i in Spp){
-sp <- filter(ibp, Species %in% i) %>% st_as_sf(coords=c("Lon", "Lat"), crs=4326)
-tm <- tm + tm_shape(sp, name=i) + 
-  tm_dots(col=terrain.colors(length(Spp))[which(factor(Spp)==i)])
-}
-tm 
-
+# tm <- tm_shape(acp) + tm_polygons(col = "STRATNAME", alpha = 0.5) + 
+#   tm_shape(trans1) + tm_lines() +
+#   tm_basemap(server = "Esri.WorldGrayCanvas")
+# for(i in Spp){
+# sp <- filter(ibp, Species %in% i) %>% st_as_sf(coords=c("Lon", "Lat"), crs=4326)
+# tm <- tm + tm_shape(sp, name=i) + 
+#   tm_dots(col=terrain.colors(length(Spp))[which(factor(Spp)==i)])
+# }
+# tm 
+# 
 #flocks?
-
 
 #map year-specific observations and make track lines
 Y = 2019
 df <- filter(birds, Year==Y) %>%
-  group_by(Transect) 
-ggplot(data=trans1, group=TRANSID) + 
-  geom_sf(col=trans1$TRANSID) + 
+  group_by(Transect)
+ggplot(data=trans1, group=TRANSID) +
+  geom_sf(col=trans1$TRANSID) +
   geom_sf(data=df, size=0.2)
-#leaflet
-tm <- tm_shape(acp) + tm_polygons(col = "STRATNAME", alpha = 0.5) + 
-  tm_shape(trans1, name="Design Trans.") + tm_lines() +
-  tm_text("TRANSID") +
-  tm_basemap(server = "Esri.WorldGrayCanvas") + 
-  tm_scale_bar()
-
-for(Y in 2007:2019){
-sp <- filter(birds, Year == Y) %>% st_as_sf(coords=c("Lon", "Lat"), crs=4326)
-tm <- tm + tm_shape(sp, name=as.character(Y)) + 
-    tm_dots(col=terrain.colors(13)[which(2007:2019==Y)])
-}
-tm 
-
+# #leaflet
+# tm <- tm_shape(acp) + tm_polygons(col = "STRATNAME", alpha = 0.5) + 
+#   tm_shape(trans1, name="Design Trans.") + tm_lines() +
+#   tm_text("TRANSID") +
+#   tm_basemap(server = "Esri.WorldGrayCanvas") + 
+#   tm_scale_bar()
+# 
+# for(Y in 2007:2019){
+# sp <- filter(birds, Year == Y) %>% st_as_sf(coords=c("Lon", "Lat"), crs=4326)
+# tm <- tm + tm_shape(sp, name=as.character(Y)) + 
+#     tm_dots(col=terrain.colors(13)[which(2007:2019==Y)])
+# }
+# tm 
+###################################################################
+###################################################################
 #make a linestring for each transect
 #first test on 2016 data
-test0 <- filter(birds, Year==2016) %>% 
-  cbind(st_coordinates(.)) %>%
-  #distinct(X, Y) %>%
-  group_by(Transect) %>%
-  arrange(X, .by_group=TRUE) 
-
-#make a linestring from points, first remove geometry, then rebuild
-test <- as.data.frame(test0) %>% 
-  select(-geometry) %>%
-  filter(Transect == 39) %>%
-  arrange(X, Y) %>%
-  select(X, Y) %>%
-  as.matrix() %>%
-  st_linestring() %>%
-  st_sfc(crs=4326) %>%
-  st_sf(geometry=.) %>%
-  mutate(Transect = 39)
-plot(st_geometry(test))
+# test0 <- filter(birds, Year==2016) %>% 
+#   cbind(st_coordinates(.)) %>%
+#   #distinct(X, Y) %>%
+#   group_by(Transect) %>%
+#   arrange(X, .by_group=TRUE) 
+# 
+# #make a linestring from points, first remove geometry, then rebuild
+# test <- as.data.frame(test0) %>% 
+#   select(-geometry) %>%
+#   filter(Transect == 39) %>%
+#   arrange(X, Y) %>%
+#   select(X, Y) %>%
+#   as.matrix() %>%
+#   st_linestring() %>%
+#   st_sfc(crs=3338) %>%
+#   st_sf(geometry=.) %>%
+#   mutate(Transect = 39)
+# plot(st_geometry(test))
 #that worked, now turn into function and apply across all transects
 
 points2line <- function(x, Year=unique(x$Year), Transect=unique(x$Transect), crs=4326){
   #this function accepts sf object of points and returns an sf linestring
-  #also will accept an ID variable and return that as an attribute of the linestring, 
-  # otherwise returns an ID == NA.
+  # sf object define ONE linestring, not more than one!
+  #accepted sf object should have an attribute for named Year and Transect, 
+  # if not, supply as a parameter value
   linestring <- x %>% cbind(st_coordinates(.)) %>%
     as.data.frame() %>% 
     select(-geometry) %>%
@@ -136,42 +160,50 @@ points2line <- function(x, Year=unique(x$Year), Transect=unique(x$Transect), crs
 }
 
 #test
-x = filter(birds, Year == 2016, Transect == 39)
+x = birds %>% st_transform(crs=4326) %>%
+  filter(Year == 2016, Transect == 39)
 points2line(x=x)
 
 #Now apply function across all transects
-lines <- birds %>% group_split(Year, Transect) %>%
+lines <- birds %>% st_transform(crs=4326) %>%
+  group_split(Year, Transect, Day) %>%
   map(points2line) %>%
   map_dfr(rbind)
-#plot them
-filter(lines, Year==2010) %>%
+#plot them, filter through years and plot each year
+filter(lines, Year==2019) %>%
   st_geometry() %>%
   plot()
-# found some QC issues, plot 2010, 2013, 2015 2016 2017 2019 data and others
-  
+# found some QC issues, plot 2010, 2013, 2014, 2015, 2016 2017 2018 2019 data and others
+################################################################################
+## Observation quality control process starts here
+#cycle through years to discover QC issues and propose solutions, see below  
 #plot in leaflet
-Y = 2007
+Y = 2013
+# Tran = 1453
+# df <- filter(lines, Year==Y, Transect==Tran)
+# bdf <- filter(birds, Year==Y, Transect==Tran) %>% mutate(Day=as.character(Day))
 df <- filter(lines, Year==Y)
-bdf <- filter(birds, Year==Y)
+bdf <- filter(birds, Year==Y) %>% mutate(Day=as.character(Day))
 tm <- tm_shape(acp) + tm_polygons(col = "STRATNAME", alpha = 0.5) + 
   tm_shape(df, name=paste(Y, "Flown Track")) + tm_lines() + 
-  tm_text("Transect") + 
-  tm_shape(bdf, name=paste(Y,"Bird Obs")) + tm_dots() + 
+  tm_text("Transect", size=2) + 
+  tm_shape(bdf, name=paste(Y,"Bird Obs")) + tm_dots(col="Day") + 
   tm_basemap(server = "Esri.WorldGrayCanvas") + 
   tm_scale_bar() 
+tm
 
 #loop through years
-tm <- tm_shape(acp) + tm_polygons(col = "STRATNAME", alpha = 0.5)
-for(Y in 2007:2019){
-  df <- filter(lines, Year==Y)
-  bdf <- filter(birds, Year==Y)
-  tm <- tm + tm_shape(df, name=paste(Y, "Flown Track")) + tm_lines() + 
-    tm_text("Transect") + 
-  tm_shape(bdf, name=paste(Y,"Bird Obs")) + tm_dots() + 
-  tm_basemap(server = "Esri.WorldGrayCanvas") + 
-  tm_scale_bar() 
-}
-tm 
+# tm <- tm_shape(acp) + tm_polygons(col = "STRATNAME", alpha = 0.5)
+# for(Y in 2007:2019){
+#   df <- filter(lines, Year==Y)
+#   bdf <- filter(birds, Year==Y)
+#   tm <- tm + tm_shape(df, name=paste(Y, "Flown Track")) + tm_lines() + 
+#     tm_text("Transect") + 
+#   tm_shape(bdf, name=paste(Y,"Bird Obs")) + tm_dots() + 
+#   tm_basemap(server = "Esri.WorldGrayCanvas") + 
+#   tm_scale_bar() 
+# }
+# tm 
 #hide layers
 # %>% 
 #   tmap_leaflet() %>%
@@ -179,8 +211,82 @@ tm
 
 #QC issues with transect numbering, try sorting by time?
 
+# ##function to calculate speed based on position and time as recorded in data
+# # used as a QC step to identify problematic points
+# # according the HMW plausible speeds are 45 - 150 mph
+# speed <- function(x){
+#  #x is a sf data frame with point geometry and a time variable for the time of recording for each point
+#  df <- x %>% group_by(Transect, Day, Observer) %>%
+#    slice(-n())
+#  df2 <- x %>% group_by(Transect, Day, Observer) %>%
+#    slice(-1)
+#  
+#  df3 <- st_distance(x=df, y=df2, by_element = TRUE)
+#  dTime <- df2$Time - df$Time
+#  return( as.vector( (60*60*df3) / (dTime*1000) ) )
+# }
+# #apply to all data for one year, 2017
+# x <- data.frame(Speed = as.vector(speed(x = bdf)/1.61) ) %>% #miles per hour
+#   drop_na()
+# hist(x$Speed[x$Speed < 300], xlim=c(-200, 300), breaks=100)
+# summary(x$Speed[x$Speed < Inf])
+
+################################
+##QC birds data set
+# structure of below:
+## QC find: Year = XXXX, Transect = XX, Description = short English description. 
+## Solution: English description of solution
+## then tidyverse (or general R) code
+birdsQC <- birds %>% cbind(st_coordinates(.)) %>%
+  ## QC find: Year = 2010, Transect = 1453, Description = small number of observations (5) attributed to transect 1453 have lat/longs associated with transect 363. 363 seem to have been done later on 2010-19.
+  ## Solution: delete five observations. These obs are very far west on 363, so delete based on transect number and long < 0.
+  filter(!(Year == 2010 & Transect == 1453 & X < 0) ) %>%
+  ## QC find: Year = 2013, Transect = 38 and 45, Description = 
+  #
+  # design <- st_read(dsn=paste("Design_Transects/ACP_",Y,"_Transects.shp", sep="")) %>%
+  # st_transform(crs=3338)
+  # birdsQC <- birds %>% cbind(st_coordinates(.)) %>% filter(Year == 2013, Transect %in%c(38, 45))
+  # df <- filter(design, TRANSID %in% c(38, 45)) %>% st_distance(birdsQC) %>% t() %>%
+  # as.data.frame() %>% rename(to_38 = V1, to_45=V2) %>%
+  # bind_cols(birdsQC)
+  # view(birdQC)
+  # HMW seems to have reversed transect 38 and 45 on day 10
+  ## Solution: reverse transect 38 and 45 on day 10
+  mutate(Transect = if_else(Year == 2013 & Observer == "HMW" & Day == 10, 
+                            if_else(Transect == 38, 45, 38), 
+                            Transect))
+#Did it work?
+
+#plots for QC
+Y = 2013
+df <- filter(lines, Year==Y)
+bdf <- birdsQC %>% filter(Year==Y) %>% mutate(Day=as.character(Day))
+tm <- tm_shape(acp) + tm_polygons(col = "STRATNAME", alpha = 0.5) + 
+  tm_shape(df, name=paste(Y, "Flown Track")) + tm_lines() + 
+  tm_text("Transect", size=2) + 
+  tm_shape(bdf, name=paste(Y,"Bird Obs")) + tm_dots(col="Observer") + 
+  tm_basemap(server = "Esri.WorldGrayCanvas") + 
+  tm_scale_bar() 
+tm
 
 
-#segmentize transects
+
+#segmenting transects
 source("trans2seg.R")
-trans.seg <- trans2seg(trans1, utm=6332)
+#need to remove duplicate points (when multiple birds are seen at the same point)
+test2 <- as.data.frame(test0) %>% 
+  select(-geometry) %>%
+  #filter(Transect == 39) %>%
+  arrange(X, Y) %>%
+  select(X, Y) %>%
+  distinct() %>% #this removes duplicate points
+  as.matrix() %>%
+  st_linestring() %>%
+  st_sfc(crs=4326) %>%
+  st_sf(geometry=.) 
+#%>%
+#  mutate(Transect = 39)
+trans.seg <- trans2seg(test2, utm=6332)
+#NOTE: after/during segmentization need to id original vs. segmentized points and remove original points. 
+#hummm, this is too hard. how st_segmetize just adds points tot he linestring. I need to use
+# the observed point to re-create the track, then chuck it up. 
