ae69606dfba88e1c811bd57715805ca2eadb6616
Erik Osnas <erik_osnas@fws.gov>
2022-05-26 11:07:42 -0800

    add QC steps through 2014, currently in discovery for 2015.

--- ACPmapping.R
+++ ACPmapping.R
@@ -69,7 +69,8 @@ birds <- map(csv_files, read_csv, col_types=cols(Transect=col_double())) %>%
           "Observer", "Species", "Num", "Obs_Type", "Code", "Lat", "Lon") %>%
   st_as_sf(coords=c("Lon", "Lat"), crs=4326) %>%
   st_transform(crs=3338) %>%
-  st_filter(bbox) #get rid of the birds in Nigeria (lat/long = 0), only 14 observations
+  st_filter(bbox) %>% #get rid of the birds in Nigeria (lat/long = 0), only 14 observations
+  filter(Code == 1) #remove any non-survey or special observations 
 
 # ibp <- birds %>% filter(Obs_Type != "flkdrake", Num > 0,
 #                         Obs_Type %in% c("single", "pair") | (Obs_Type == "open" & Num < 3))
@@ -211,20 +212,21 @@ tm
 
 #QC issues with transect numbering, try sorting by time?
 
-# ##function to calculate speed based on position and time as recorded in data
-# # used as a QC step to identify problematic points
-# # according the HMW plausible speeds are 45 - 150 mph
-# speed <- function(x){
-#  #x is a sf data frame with point geometry and a time variable for the time of recording for each point
-#  df <- x %>% group_by(Transect, Day, Observer) %>%
-#    slice(-n())
-#  df2 <- x %>% group_by(Transect, Day, Observer) %>%
-#    slice(-1)
-#  
-#  df3 <- st_distance(x=df, y=df2, by_element = TRUE)
-#  dTime <- df2$Time - df$Time
-#  return( as.vector( (60*60*df3) / (dTime*1000) ) )
-# }
+
+speed <- function(x){
+ ##function to calculate speed based on position and time as recorded in data
+ # used as a QC step to identify problematic points
+ # according the HMW plausible speeds are 45 - 150 mph
+ #x is a sf data frame with point geometry and a time variable for the time of recording for each point
+ df <- x %>% group_by(Transect, Day, Observer) %>%
+   slice(-n())
+ df2 <- x %>% group_by(Transect, Day, Observer) %>%
+   slice(-1)
+
+ df3 <- st_distance(x=df, y=df2, by_element = TRUE)
+ dTime <- df2$Time - df$Time
+ return( as.vector( (60*60*df3) / (dTime*1000) ) )
+}
 # #apply to all data for one year, 2017
 # x <- data.frame(Speed = as.vector(speed(x = bdf)/1.61) ) %>% #miles per hour
 #   drop_na()
@@ -241,52 +243,151 @@ birdsQC <- birds %>% cbind(st_coordinates(.)) %>%
   ## QC find: Year = 2010, Transect = 1453, Description = small number of observations (5) attributed to transect 1453 have lat/longs associated with transect 363. 363 seem to have been done later on 2010-19.
   ## Solution: delete five observations. These obs are very far west on 363, so delete based on transect number and long < 0.
   filter(!(Year == 2010 & Transect == 1453 & X < 0) ) %>%
-  ## QC find: Year = 2013, Transect = 38 and 45, Description = 
-  #
-  # design <- st_read(dsn=paste("Design_Transects/ACP_",Y,"_Transects.shp", sep="")) %>%
-  # st_transform(crs=3338)
-  # birdsQC <- birds %>% cbind(st_coordinates(.)) %>% filter(Year == 2013, Transect %in%c(38, 45))
-  # df <- filter(design, TRANSID %in% c(38, 45)) %>% st_distance(birdsQC) %>% t() %>%
-  # as.data.frame() %>% rename(to_38 = V1, to_45=V2) %>%
-  # bind_cols(birdsQC)
-  # view(birdQC)
-  # HMW seems to have reversed transect 38 and 45 on day 10
-  ## Solution: reverse transect 38 and 45 on day 10
-  mutate(Transect = if_else(Year == 2013 & Observer == "HMW" & Day == 10, 
-                            if_else(Transect == 38, 45, 38), 
-                            Transect))
+  select(!c("X", "Y"))
+## QC find: Year = 2013, Transect = 38 and 45, Description = 
+#
+#   trans <- st_read(dsn="Design_Transects/ACP_2013_Transects.shp") %>%
+#   st_transform(crs=3338)
+# plot(st_geometry(acp3))
+# plot(st_geometry(filter(trans, TRANSID == 45)), add = TRUE)
+# plot(st_geometry(filter(birds, Year == 2013 & Day == 10 & Observer == "HMW" & Transect == 45)),
+#      add = TRUE)
+# plot(st_geometry(filter(birds, Year == 2013 & Day == 10 & Observer == "WWL" & Transect == 45)), 
+#      add = TRUE)
+# plot(st_geometry(filter(trans, TRANSID == 38)), col="red", add = TRUE)
+# 
+# plot(st_geometry(acp3))
+# plot(st_geometry(filter(trans, TRANSID == 38)), add = TRUE)
+# plot(st_geometry(filter(birds, Year == 2013 & Day == 10 & Observer == "HMW" & Transect == 38)),
+#      add = TRUE)
+# plot(st_geometry(filter(birds, Year == 2013 & Day == 10 & Observer == "WWL" & Transect == 38)), 
+#      add = TRUE)
+# plot(st_geometry(filter(trans, TRANSID == 45)), col="red", add = TRUE)
+# HMW seems to have misslabled transect 38 as 45 on day 10 and
+# HMW misslabled transect 45 as 38 on day 10
+## Solution: reverse transect 38 and 45 for HMW on day 10; need to create temporary df to keep results
+df <- birdsQC %>% filter(Year == 2013 & Observer == "HMW" & Day == 10 & Transect == 38) %>% 
+  mutate(Transect = 45)
+df2 <- birdsQC %>% filter(Year == 2013 & Observer == "HMW" & Day == 10 & Transect == 45) %>% 
+  mutate(Transect = 38)
+birdsQC <- birdsQC %>% filter(!(Year == 2013 & Observer == "HMW" & Day == 10 & Transect %in%c(38, 45))) %>%
+  bind_rows(df, df2)
+          
 #Did it work?
+trans <- st_read(dsn="Design_Transects/ACP_2013_Transects.shp") %>%
+  st_transform(crs=3338)
+plot(st_geometry(acp3))
+plot(st_geometry(filter(trans, TRANSID == 45)), add = TRUE)
+df <- birdsQC
+plot(st_geometry(filter(df, Year == 2013 & Day == 10 & Observer == "HMW" & Transect == 45)),
+     add = TRUE)
+plot(st_geometry(filter(df, Year == 2013 & Day == 10 & Observer == "WWL" & Transect == 45)),
+     add = TRUE)
+plot(st_geometry(filter(trans, TRANSID == 38)), col="red", add = TRUE)
 
+plot(st_geometry(filter(df, Year == 2013 & Day == 10 & Observer == "HMW" & Transect == 38)),
+     col="red", add = TRUE)
+plot(st_geometry(filter(df, Year == 2013 & Day == 10 & Observer == "WWL" & Transect == 38)),
+     col="red", add = TRUE)
+# Yes!
+# recompute lines
+lines <- birdsQC %>% st_transform(crs=4326) %>%
+  group_split(Year, Transect, Day) %>%
+  map(points2line) %>%
+  map_dfr(rbind)
+#Plot seem to suggest it worked
+# continue QC
 #plots for QC
-Y = 2013
+Y <- 2014
 df <- filter(lines, Year==Y)
-bdf <- birdsQC %>% filter(Year==Y) %>% mutate(Day=as.character(Day))
+plot(st_geometry(df))
+bdf <- birdsQC %>% filter(Year == Y) %>% mutate(Day=as.character(Day))
 tm <- tm_shape(acp) + tm_polygons(col = "STRATNAME", alpha = 0.5) + 
   tm_shape(df, name=paste(Y, "Flown Track")) + tm_lines() + 
   tm_text("Transect", size=2) + 
-  tm_shape(bdf, name=paste(Y,"Bird Obs")) + tm_dots(col="Observer") + 
-  tm_basemap(server = "Esri.WorldGrayCanvas") + 
-  tm_scale_bar() 
+  tm_shape(bdf, name=paste(Y,"Bird Obs")) + tm_dots(col="Time") + 
+  #tm_basemap(server = "Esri.WorldImagery") + 
+  tm_basemap(server = "Esri.WorldGrayCanvas")
+tm
+# QC find:  Year = 2014, Transect = 43, Description = 
+#  It appears a section of transect 43 was sampled twice, once on day ==11 and again on day ==12.
+#  It seems implied that search effort was stopped before "end transect" there was recorded or computer/GPS failure 
+#  bdf <- filter(birdsQC, Year == 2014 & Transect ==43)
+# Solution: renumber transects so that each segment is a unique number
+birdsQC <- birdsQC %>% 
+#  transect 43 on Day = 12 becomes 43.3
+#  transect 43 on Day = 11 and time < 73597 (west side) becomes 43.1
+#  transect 43 on Day = 11 and time >74000 (east side) becomes 43.2
+  mutate(Transect = if_else(Year == 2014 & Transect == 43 & Day == "12", 43.3, Transect)) %>% 
+  mutate(Transect = if_else(Year == 2014 & Transect == 43 & Day == "11" & Time < 73597, 43.1, Transect)) %>%
+  mutate(Transect = if_else(Year == 2014 & Transect == 43 & Day == "11" & Time > 74000, 43.2, Transect))
+# recompute lines
+lines <- birdsQC %>% st_transform(crs=4326) %>%
+  group_split(Year, Transect, Day) %>%
+  map(points2line) %>%
+  map_dfr(rbind)
+#Plot seem to suggest it worked
+# continue QC
+#plots for QC
+Y <- 2014
+df <- filter(lines, Year==Y)
+# #does calculating speed and displaying color of dots based on speed help QC?
+# bdf <- birdsQC %>% filter(Year == Y) %>% mutate(Day=as.character(Day))
+# x <- data.frame(Speed=speed(bdf)/1.61)
+# sdf <- bdf %>% group_by(Transect, Day, Observer) %>%
+#   slice(-n()) %>% bind_cols(x) %>% 
+#   mutate("Speed Bin" = ifelse(Speed < 45, "too slow", ifelse(Speed > 150, "too fast","just right"))) %>%
+#   filter(Speed < 45 | Speed > 150)
+# tm <- tm_shape(acp) + tm_polygons(col = "STRATNAME", alpha = 0.5) + 
+#   tm_shape(df, name=paste(Y, "Flown Track")) + tm_lines() + 
+#   tm_text("Transect", size=2) + 
+#   tm_shape(sdf, name=paste(Y,"Bird Obs")) + tm_dots(col="Speed Bin", size = 0.1) + 
+#   #tm_basemap(server = "Esri.WorldImagery") + 
+#   tm_basemap(server = "Esri.WorldGrayCanvas")
+# tm
+# #Maybe, mostly IDs points of start or end with same location as birds obs. 
+# QC find: Transect 35, Year = 2014, Descrption = Transect ended at ~65980, then started again
+# QC soluton: assign segements
+birdsQC <- birdsQC %>% 
+  mutate(Transect = if_else(Year == 2014 & Transect == 35 & Time < 65990, 35.1, Transect)) %>%
+  mutate(Transect = if_else(Year == 2014 & Transect == 35 & Time > 65990, 35.2, Transect))
+# recompute lines
+lines <- birdsQC %>% st_transform(crs=4326) %>%
+  group_split(Year, Transect, Day) %>%
+  map(points2line) %>%
+  map_dfr(rbind)
+#continue mapping transects
+Y <- 2015
+df <- filter(lines, Year==Y)
+bdf <- birdsQC %>% filter(Year == Y) %>% mutate(Day=as.character(Day))
+tm <- tm_shape(acp) + tm_polygons(col = "STRATNAME", alpha = 0.5) + 
+  tm_shape(df, name=paste(Y, "Flown Track")) + tm_lines() + 
+  tm_text("Transect", size=2) + 
+  tm_shape(bdf, name=paste(Y,"Bird Obs")) + tm_dots(col="Time") + 
+  #tm_basemap(server = "Esri.WorldImagery") + 
+  tm_basemap(server = "Esri.WorldGrayCanvas")
 tm
 
+#QC find: Year 2015, Transect 24 and 39
+#  Transect 14 has start stop within same day, multiple occasions; also transect 13
 
 
 #segmenting transects
-source("trans2seg.R")
-#need to remove duplicate points (when multiple birds are seen at the same point)
-test2 <- as.data.frame(test0) %>% 
-  select(-geometry) %>%
-  #filter(Transect == 39) %>%
-  arrange(X, Y) %>%
-  select(X, Y) %>%
-  distinct() %>% #this removes duplicate points
-  as.matrix() %>%
-  st_linestring() %>%
-  st_sfc(crs=4326) %>%
-  st_sf(geometry=.) 
-#%>%
-#  mutate(Transect = 39)
-trans.seg <- trans2seg(test2, utm=6332)
-#NOTE: after/during segmentization need to id original vs. segmentized points and remove original points. 
-#hummm, this is too hard. how st_segmetize just adds points tot he linestring. I need to use
-# the observed point to re-create the track, then chuck it up. 
+# source("trans2seg.R")
+# #need to remove duplicate points (when multiple birds are seen at the same point)
+# test2 <- as.data.frame(test0) %>% 
+#   select(-geometry) %>%
+#   #filter(Transect == 39) %>%
+#   arrange(X, Y) %>%
+#   select(X, Y) %>%
+#   distinct() %>% #this removes duplicate points
+#   as.matrix() %>%
+#   st_linestring() %>%
+#   st_sfc(crs=4326) %>%
+#   st_sf(geometry=.) 
+# #%>%
+# #  mutate(Transect = 39)
+# trans.seg <- trans2seg(test2, utm=6332)
+# #NOTE: after/during segmentization need to id original vs. segmentized points and remove original points. 
+# #hummm, this is too hard. how st_segmetize just adds points tot he linestring. I need to use
+# # the observed point to re-create the track, then chuck it up. 
