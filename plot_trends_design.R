e844ac6e0981bcc4f687252eb72951b8e13f14e6
Erik Osnas <erik_osnas@fws.gov>
2025-02-06 11:42:40 -0900

    added posterior probability for trend

--- plot_trends_design.R
+++ plot_trends_design.R
@@ -3,34 +3,48 @@
 # include it here just for dev purposes
 ##Now lets loop through all species and write figures and data to a directory
 #will try to make this a function at some point
+library(AKaerial)
+library(tidyverse)
+library(mgcv)
 #write error and messages to log file 
 zz <- file("plot_trends/data/log.txt", open="wt")
 sink(zz ,type = "output")
 sink(zz, type = "message")
 
 path = NULL
-exclude.spp = c("CORA", "SWANN", "UNGR", "UNME", "UNEI", "BAEA", "COME")
+exclude.spp = c("SWANN", "UNGR", "UNME", "UNEI", "BAEA", "COME")
+spp = "ARTE"
 area = "ACP"
-library(AKaerial)
-library(tidyverse)
-library(mgcv)
-
+override = NULL
 if(area == "ACP") data <- ACPHistoric$output.table
 if(area == "YKD") data <- YKDHistoric$output.table
 df <- data |>
   filter(! Species %in% c("START", "END", exclude.spp)) |>
-  mutate(Species = factor(Species), ibbtotal.est = units::drop_units(ibbtotal.est), 
-         cv.ibb = SE.ibb/ibbtotal.est) |>
-  select(Year, Species, Observer, ibbtotal.est, SE.ibb, cv.ibb) |>
-  drop_na() |>
-  mutate(upper = ibbtotal.est + 2*SE.ibb, lower = ibbtotal.est - 2*SE.ibb, 
-         Observer = factor(Observer))
+  mutate(Species = factor(Species))
 #start loop
 for(spp in levels(df$Species)){
   print(spp)
   #select species and do sims
   #spp <- "SPEI"
-  sppdf <- df |> filter(Species == spp)
+  if(is.null(override)){
+    what = ifelse(spp %in% c("SNGO", "SWAN", "UNSC", "RNGR", "JAEG", "SAGU", "GLGU", 
+                            "ARTE", "RTLO", "PALO", "YBLO", "GOEA", "SEOW", "SNOW", 
+                            "CORA"), "total.est", 
+                 ifelse(spp %in% c("GWFG", "BRAN", "CCGO", "NSHO", "AMWI", "MALL", 
+                                   "NOPI", "GWTE", "STEI", "SPEI", "KIEI", "COEI", 
+                                   "SUSC", "WWSC", "BLSC", "LTDU", "RBME", "SACR"), 
+                        "itotal.est", "ibbtotal.est"))
+  }
+  se.what = ifelse(what == "ibbtotal.est", "SE.ibb", 
+                    ifelse(what == "total.est", "SE", 
+                           ifelse(what == "itotal.est", "SE.i", NULL)))
+  sppdf <- df |> filter(Species == spp) |>
+    mutate(index = .data[[what]], se.index = .data[[se.what]]) |>
+    mutate(index = units::drop_units(index), cv = se.index/index) |>
+    select(Year, Species, Observer, index, se.index, cv) |>
+    drop_na() |>
+    mutate(upper = index + 2*se.index, lower = index - 2*se.index, 
+           Observer = factor(Observer))
   yearobs <- length(unique(sppdf$Year))
   NumObs <- length(unique(sppdf$Observer)) 
   kyear <- ifelse(length(sppdf$Year) > yearobs - 1 + NumObs, 
@@ -42,13 +56,13 @@ for(spp in levels(df$Species)){
     next()
   }
   if(kyear < yearobs -1) message(paste("Number of temporal knots for", spp,"restricted to", kyear))
-  nreps = 200
+  nreps = 300
   results <- w <- vector("list", nreps)
   newdf <- sppdf |> mutate(Observer = "HMW") |> select(Year, Species, Observer) |> 
     distinct()
   for(i in 1:nreps){
     #need to catch NaNs for rare or poorly estimated species
-    sppdf$est <- log(rnorm(dim(sppdf)[1], mean = sppdf$ibbtotal.est, sd = sppdf$SE.ibb))
+    sppdf$est <- log(rnorm(dim(sppdf)[1], mean = sppdf$index, sd = sppdf$se.index))
     sppdf$est <- if_else(is.na(sppdf$est), 1e-10, sppdf$est)
     bootfit <- gam(est ~ s(Year, bs = "tp", k = kyear) +
                      s(Observer, bs = "re"),
@@ -68,10 +82,10 @@ for(spp in levels(df$Species)){
     pivot_longer(4:(4+nreps-1), names_to = "Rep", values_to = "results") 
   # ggplot(data = repdf) + 
   #   geom_line(aes(x = Year, y = results, group = Rep), alpha = 0.25) + 
-  #   geom_pointrange(data = sppdf, aes(x = Year, y = ibbtotal.est, ymin = lower, ymax = upper, 
+  #   geom_pointrange(data = sppdf, aes(x = Year, y = index, ymin = lower, ymax = upper, 
   #                                     col = Observer))
   #fit at point estimate to compare
-  # sppdf$logest <- log(sppdf$ibbtotal.est)
+  # sppdf$logest <- log(sppdf$index)
   # fit1 <- gam(logest ~ s(Year, bs = "tp", k = kyear) +
   #               s(Observer, bs = "re"),
   #             method = "REML", data = sppdf, family = scat)
@@ -82,21 +96,21 @@ for(spp in levels(df$Species)){
   #   geom_ribbon(data = fitdf, aes(x=Year, ymin=lower, ymax = upper, fill = "orange")) + 
   #   geom_line(data = repdf, aes(x = Year, y = results, group = Rep), alpha = 0.25) + 
   #   geom_line(data = fitdf, aes(x = Year, y = fit), col = "white", lwd = 1) + 
-  #   geom_pointrange(data = sppdf, aes(x = Year, y = ibbtotal.est, ymin = lower, ymax = upper, 
+  #   geom_pointrange(data = sppdf, aes(x = Year, y = index, ymin = lower, ymax = upper, 
   #                                     col = Observer))
   #Now use results to find mean and SD at each time point
   sumdf <- repdf |> group_by(Year) |>
     summarise(mean = mean(results), sd = sd(results)) |>
-    mutate(upper = mean + 2*sd, lower = mean - 2*sd)
+    mutate(index = what, upper = mean + 2*sd, lower = mean - 2*sd)
   write_csv(sumdf, file =  paste0(path, "plot_trends/data/", spp, "_bootfit.csv"))
   p1 <- ggplot() + 
     geom_ribbon(data = sumdf, aes(x=Year, ymin=lower, ymax = upper, fill = "orange"), 
                 show.legend = FALSE) + 
     geom_line(data = repdf, aes(x = Year, y = results, group = Rep), alpha = 0.25) + 
     geom_line(data = sumdf, aes(x = Year, y = mean), col = "white", lwd = 1) + 
-    geom_pointrange(data = sppdf, aes(x = Year, y = ibbtotal.est, ymin = lower, ymax = upper, 
+    geom_pointrange(data = sppdf, aes(x = Year, y = index, ymin = lower, ymax = upper, 
                                       col = Observer), show.legend = FALSE) + 
-    labs(title = paste(spp, "on", area), y = "IBBtotal")
+    labs(title = paste(spp, "on", area), y = what)
   png(filename = paste0(path, "plot_trends/figures/", spp, "_index.png"))
   print(p1)
   dev.off()
@@ -116,11 +130,14 @@ for(spp in levels(df$Species)){
   }
   #hist(filter(trendDF, Lag == 35)$Trend)
   #summarize and plot
-  sumTrend <- trendDF |> group_by(Lag) |>
-    summarise(mean = mean(Trend), 
+  sumTrend <- trendDF |> 
+    mutate(P = Trend < 1) |>
+    group_by(Lag) |>
+    summarise(index = what, mean = mean(Trend), 
               median = quantile(Trend, probs = 0.5), 
               lower = quantile(Trend, probs = 0.025), 
-              upper = quantile(Trend, probs = 0.975))
+              upper = quantile(Trend, probs = 0.975), 
+              P = mean(P))
   write_csv(sumTrend, file =  paste0(path, "plot_trends/data/", spp, "_trend.csv"))
   p2 <- ggplot(data = sumTrend) + 
     geom_ribbon(aes(x=Lag, ymin=lower, ymax = upper), fill = "orange") + 
@@ -132,6 +149,8 @@ for(spp in levels(df$Species)){
   print(p2)
   dev.off()
 }
+print(warnings())
 sink(type = "output")
 sink(type="message")
 close(zz)
+
